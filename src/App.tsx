3e275a514fd5d62abb674b2d03a012cb0427e1e0
Ruslan <emfree20@gmail.com>
2024-10-07 15:43:32 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,42 @@
-import { Navigation } from '@components/Navigation/Navigation';
-import { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigation } from '@components/Navigation/Navigation'
+import { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
 
 declare global {
   interface Window {
-    Telegram: any;
+    Telegram: any
   }
 }
 
-const tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp
 
 export const App = () => {
-  const [userData, setUserData] = useState<{ name?: string; surname?: string; id?: string }>({});
+  const [data, setData] = useState({});
 
   useEffect(() => {
     const firstLayerInitData = Object.fromEntries(
-      new URLSearchParams(window.Telegram.WebApp.initData)
+      new URLSearchParams(window.Telegram.WebApp.initDataUnsafe)
     );
 
     const initData: Record<string, string> = {};
 
-    for (const key in firstLayerInitData) {
-      try {
-        initData[key] = JSON.parse(firstLayerInitData[key]);
-      } catch {
-        initData[key] = firstLayerInitData[key];
-      }
-    }
+    // for (const key in firstLayerInitData) {
+    //   try {
+    //     initData[key] = JSON.parse(firstLayerInitData[key]);
+    //   } catch {
+    //     initData[key] = firstLayerInitData[key];
+    //   }
+    // }
 
-    // Extracting name, surname, and id from initData
-    const { first_name, last_name, id } = initData;
-    setUserData({
-      name: first_name,
-      surname: last_name,
-      id: id,
-    });
+    setData(initData);
 
-    console.log(initData);
+    console.log(data)
   }, []);
 
   return (
     <div>
-      <p>
-        {userData.name && userData.surname
-          ? `Name: ${userData.name}, Surname: ${userData.surname}, ID: ${userData.id}`
-          : 'Loading...'}
-      </p>
-      <button onClick={() => tg.close()}>Close</button>
+     <p>{Object.keys(data).length > 0 ? JSON.stringify(data) : 'Loading...'}</p>
+      <button onClick={() => tg.close()}>close</button>
       <main>
         <Outlet />
       </main>

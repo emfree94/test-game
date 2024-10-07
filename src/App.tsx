e6a158e941f8a96b9350c986b5e73cb21084c309
Ruslan <emfree20@gmail.com>
2024-10-07 14:56:51 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,16 @@ const tg = window.Telegram.WebApp
 export const App = () => {
   useEffect(() => {
     tg.ready()
-    console.log('tg.initDataUnsafe?.user?', tg.initData)
+    console.log('tg.initDataUnsafe?.user?', tg.initDataUnsafe)
   }, [])
 
   return (
     <div>
-      <p>{tg.initDataUnsafe?.user?.username}</p>
+      <p>
+        {tg.initDataUnsafe?.user?.username
+          ? tg.initDataUnsafe.user.username
+          : 'Loading...'}
+      </p>
       <button onClick={() => tg.close()}>close</button>
       <main>
         <Outlet />

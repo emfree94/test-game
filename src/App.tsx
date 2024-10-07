99263a25ef5cf172739f126b07f836035760dab8
Ruslan <emfree20@gmail.com>
2024-10-07 11:57:37 +0300

    fix err

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { Navigation } from '@components/Navigation/Navigation'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
 declare global {
   interface Window {
-      Telegram: any;
+    Telegram: any
+  }
 }
 
-
 export const App = () => {
-  const [telegramData, setTelegramData] = useState(null);
+  const [telegramData, setTelegramData] = useState<URLSearchParams | null>(null)
 
   useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      const initData = window.Telegram.WebApp.initData;  // Отримуємо дані від Telegram
-      const params = new URLSearchParams(initData);      // Парсимо URL-параметри
-      setTelegramData(params);                           // Зберігаємо дані в стан
+    if (
+      window.Telegram &&
+      window.Telegram.WebApp &&
+      window.Telegram.WebApp.initData
+    ) {
+      const initData = window.Telegram.WebApp.initData // Get the data from Telegram
+      const params = new URLSearchParams(initData) // Parse URL parameters
+      setTelegramData(params) // Save data to state
     }
-  }, []);
+  }, [])
 
   console.log(telegramData)
 
@@ -29,17 +33,17 @@ export const App = () => {
       <Navigation />
 
       <div>
-      {telegramData ? (
-        <div>
-          <p>User ID: {telegramData.get('id')}</p>
-          <p>First Name: {telegramData.get('first_name')}</p>
-          <p>Last Name: {telegramData.get('last_name')}</p>
-          <p>Username: {telegramData.get('username')}</p>
-        </div>
-      ) : (
-        <p>No Telegram data available.</p>
-      )}
-    </div>
+        {telegramData ? (
+          <div>
+            <p>User ID: {telegramData.get('id')}</p>
+            <p>First Name: {telegramData.get('first_name')}</p>
+            <p>Last Name: {telegramData.get('last_name')}</p>
+            <p>Username: {telegramData.get('username')}</p>
+          </div>
+        ) : (
+          <p>No Telegram data available.</p>
+        )}
+      </div>
     </div>
   )
 }

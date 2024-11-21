7683f4e515d04454798a46d67f576b1a0afa12c9
Ruslan <emfree20@gmail.com>
2024-11-21 13:02:06 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,27 @@
-import { Navigation } from '@components/Navigation/Navigation';
-import { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigation } from '@components/Navigation/Navigation'
+import { useEffect, useState } from 'react'
+import { Outlet } from 'react-router-dom'
 
 declare global {
   interface Window {
-    Telegram: any;
+    Telegram: any
   }
 }
 
-const tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp
 
 export const App = () => {
-  const [data, setData] = useState<Record<string, any>>({});
-  const [rawInitData, setRawInitData] = useState<string>("");
+  const [rawInitData, setRawInitData] = useState(null)
 
   useEffect(() => {
-    const firstLayerInitData = Object.fromEntries(
-      new URLSearchParams(window.Telegram.WebApp.initData)
-    );
-
-    const initData: Record<string, string> = {};
-
-    for (const key in firstLayerInitData) {
-      try {
-        initData[key] = JSON.parse(firstLayerInitData[key]);
-      } catch {
-        initData[key] = firstLayerInitData[key];
-      }
-    }
-
-    setData(initData);
-    setRawInitData(window.Telegram.WebApp.initData);
-    console.log(initData);
-  }, []);
+    setRawInitData(tg.initData)
+  }, [])
 
   return (
     <div>
       <div>
-        <h3>Parsed Init Data:</h3>
-        {Object.keys(data).length > 0 ? (
-          Object.entries(data).map(([key, value]) => (
-            <p key={key}>
-              <strong>{key}:</strong>{' '}
-              {typeof value === 'object' ? JSON.stringify(value) : value}
-            </p>
-          ))
-        ) : (
-          'Loading...'
-        )}
-      </div>
-      <div>
         <h3>Raw Init Data (JSON):</h3>
-        <pre>{JSON.stringify(rawInitData, null, 2)}</pre> 
+        <pre>{JSON.stringify(rawInitData)}</pre>
       </div>
       <button onClick={() => tg.close()}>Close</button>
       <main>
@@ -59,5 +29,5 @@ export const App = () => {
       </main>
       <Navigation />
     </div>
-  );
-};
+  )
+}

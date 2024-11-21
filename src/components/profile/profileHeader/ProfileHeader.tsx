b59cb2488ab5b3c4876d33f1e143450310ab0f5b
Ruslan <emfree20@gmail.com>
2024-11-21 16:52:28 +0200

    test

--- src/components/profile/profileHeader/ProfileHeader.tsx
+++ src/components/profile/profileHeader/ProfileHeader.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import avatar from '@assets/icon/avatar.svg'
+import avatarImg from '@assets/icon/avatar.svg'
 import ratingIcon from '@assets/icon/rating.svg'
 import './profileHeader.scss'
 import { useSelector } from 'react-redux'
@@ -10,13 +10,13 @@ export const ProfileHeader = (): JSX.Element => {
   return (
     <div className="profile-header">
       <div className="avatar">
-        <img src={avatar} alt="user-avatar" />
+        <img src={avatar || avatarImg} alt="user-avatar" />
       </div>
       <div className="user-rating">
         <img src={ratingIcon} alt="user-rating" />
         <p className="rating text-tiny-regular">33</p>
       </div>
-      <h1 className="nickname title-bold ">{name}</h1>
+      <h1 className="nickname title-bold ">{name || 'NickName'}</h1>
     </div>
   )
 }
--- src/features/response/responseSlice.ts
+++ src/features/response/responseSlice.ts
@@ -1,12 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+interface Notifications {
+  email: boolean;
+  sms: boolean;
+  telegram: boolean;
+}
+
+interface Settings {
+  language: string;
+  sound: boolean;
+  vibration: boolean;
+  notifications: Notifications;
+  timezone: string;
+}
+
+interface UserData {
+  id: number;
+  referrer_id: number | null;
+  tg_id: number;
+  name: string;
+  email: string | null;
+  email_verified_at: string | null;
+  phone: string | null;
+  phone_verified_at: string | null;
+  country: string | null;
+  avatar: string;
+  settings: Settings;
+  token: string;
+}
+
+interface ResponseState {
+  data: UserData | null;
+}
+
+const initialState: ResponseState = {
+  data: null,
+
+}
 const responseSlice = createSlice({
   name: 'response',
-  initialState: {
-    data: null,
-  },
+  initialState,
   reducers: {
-    userData: (state, action) => {
+    userData: (state, action: PayloadAction<UserData>) => {
       state.data = action.payload;
     },
   },

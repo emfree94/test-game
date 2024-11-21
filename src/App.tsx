ae9311bacb68626d1544afe26dadad31b11d6df4
Ruslan <emfree20@gmail.com>
2024-11-21 14:18:18 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ export const App = () => {
       <div>
         <h3>Raw Init Data (JSON):</h3>
         {/* Ensure rawInitData is valid JSON or provide fallback */}
-        <pre>{rawInitData} </pre>
+        <pre> rawInitData: {rawInitData} </pre>
       </div>
       <button onClick={() => window.Telegram.WebApp.close()}>Close</button> {/* Close the Telegram WebApp */}
       <main>

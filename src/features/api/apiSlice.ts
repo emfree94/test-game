7fe306e0c7b3ff93b92dccf513e5c5593acef0dc
Ruslan <emfree20@gmail.com>
2024-11-21 15:48:16 +0200

    test

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,18 +1,19 @@
-// File: src/features/apiSlice.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.chuvachi.online/api/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.chuvachi.online/api',
+  }),
   endpoints: (builder) => ({
     postTelegramData: builder.mutation({
-      query: (body: { initData: string }) => ({
-        url: 'auth/telegram',
+      query: (initData) => ({
+        url: '/auth/telegram',
         method: 'POST',
-        body,
         headers: {
           'Content-Type': 'application/json',
         },
+        body: { initData },
       }),
     }),
   }),
